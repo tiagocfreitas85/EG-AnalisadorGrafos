a6610230d93403e22b679eed54e545ba2a4d1564
85Pedras <tiago10cf@hotmail.com>
2022-04-15 17:01:12 +0100

    falta html

--- analyser.py
+++ analyser.py
@@ -5,13 +5,14 @@ from aux import *
 class LPIS2_Interpreter(Interpreter):
     def __init__(self):
         self.regs = Registers()
-        self.instrs = {}
+        self.instrs = {'atrib': 0, 'read': 0, 'write': 0, 'if': 0, 'if_else': 0, 'for': 0, 'while': 0, 'repeat': 0}
         self.ifs = {}
         self.level = 0
         self.output = {}
 
     def start(self,tree):
-        self.visit(tree)
+        self.visit(tree.children[0])
+        self.visit_children(tree.children[2])
         self.output['regs'] = self.regs.to_string()
         self.output['instrs'] = self.instrs
         self.output['ifs'] = self.ifs
@@ -24,26 +25,91 @@ class LPIS2_Interpreter(Interpreter):
         self.visit_children(tree)
 
     def decl_int(self,tree):
-        name = tree.children[1].value
+        var = tree.children[1].value
         if len(tree.children) == 3:
-            var = self.visit(tree.children[2])
-        else: var = None
-        print(var_verifier(var,self.regs))
-        self.regs.int[name] = var
+            value = self.visit(tree.children[2])
+            if not(isinstance(value,int)):
+                if '"' in value or value == 'true' or value == 'false':
+                    print("Erro: '" + value + "' não é do tipo 'int'")
+                else:
+                    vrf = value_verifier(value,self.regs)
+                    if vrf == "A":
+                        print("Erro: Variável '" + value + "' vazia")
+                    elif vrf == "B":
+                        print("Erro: Variável '" + value + "' inexsitente")
+                    elif vrf == "C":
+                        vl = value.split('[')[0]
+                        print("Erro: '" + vl + "' não possui esse índice")
+                    elif vrf == "D":
+                        vl = value.split('[')[0]
+                        print("Erro: '" + vl + "' não possui essa entrada")
+                    elif type_verifier(var,vrf,self.regs) == 0:
+                        print("Erro: '" + value + "' não é do tipo 'int'")
+                    else:
+                        self.regs.int[var] = value
+            else:
+                self.regs.int[var] = value
+        else: 
+            value = None
+            self.regs.int[var] = value
 
     def decl_boolean(self,tree):
-        name = tree.children[1].value
+        var = tree.children[1].value
         if len(tree.children) == 3:
-            var = self.visit(tree.children[2])
-        else: var = None
-        self.regs.boolean[name] = var
+            value = self.visit(tree.children[2])
+            if not(value == 'true' or value == 'false'):
+                if isinstance(value,int) or '"' in value:
+                    print("Erro: '" + str(value) + "' não é do tipo 'boolean'")
+                else:
+                    vrf = value_verifier(value,self.regs)
+                    if vrf == "A":
+                        print("Erro: Variável '" + value + "' vazia")
+                    elif vrf == "B":
+                        print("Erro: Variável '" + value + "' inexsitente")
+                    elif vrf == "C":
+                        vl = value.split('[')[0]
+                        print("Erro: '" + vl + "' não possui esse índice")
+                    elif vrf == "D":
+                        vl = value.split('[')[0]
+                        print("Erro: '" + vl + "' não possui essa entrada")
+                    elif type_verifier(var,vrf,self.regs) == 0:
+                        print("Erro: '" + value + "' não é do tipo 'boolean'")
+                    else:
+                        self.regs.boolean[var] = value
+            else:
+                self.regs.boolean[var] = value
+        else: 
+            value = None
+            self.regs.boolean[var] = value
 
     def decl_string(self,tree):
-        name = tree.children[1].value
+        var = tree.children[1].value
         if len(tree.children) == 3:
-            var = self.visit(tree.children[2])
-        else: var = None
-        self.regs.string[name] = var
+            value = self.visit(tree.children[2])
+            if not('"' in str(value)):
+                if isinstance(value,int) or value == 'true' or value == 'false':
+                    print("Erro: '" + str(value) + "' não é do tipo 'string'")
+                else:
+                    vrf = value_verifier(value,self.regs)
+                    if vrf == "A":
+                        print("Erro: Variável '" + value + "' vazia")
+                    elif vrf == "B":
+                        print("Erro: Variável '" + value + "' inexsitente")
+                    elif vrf == "C":
+                        vl = value.split('[')[0]
+                        print("Erro: '" + vl + "' não possui esse índice")
+                    elif vrf == "D":
+                        vl = value.split('[')[0]
+                        print("Erro: '" + vl + "' não possui essa entrada")
+                    elif type_verifier(var,vrf,self.regs) == 0:
+                        print("Erro: '" + value + "' não é do tipo 'string'")
+                    else:
+                        self.regs.string[var] = value
+            else:
+                self.regs.string[var] = value
+        else: 
+            value = None
+            self.regs.string[var] = value
 
     def decl_list(self,tree):
         name = tree.children[1].value
@@ -109,7 +175,96 @@ class LPIS2_Interpreter(Interpreter):
             i += 2
         return res
 
+    def instrs(self,tree):
+        self.visit_children(tree)
 
+    def instr(self,tree):
+        if self.level > 0: self.inside += 1
+        self.visit_children(tree)
+    
+    def instr_atrib(self,tree):
+        flag = 1
+        self.instrs['atrib'] += 1
+        var = tree.children[0].value
+        value = self.visit(tree.children[1])
+        if isinstance(value,int) or ('[' not in value and '"' in value) or value == 'true' or value == 'false':
+            flag = 0
+        
+        vrf1 = var_verifier(var,self.regs)
+        vrf2 = value_verifier(value,self.regs)
+
+        if vrf1 == 3:
+            print("Erro: Variável '" + var + "' inexsitente")
+        elif vrf1 == 4:
+            print("Erro: Tuplos são imutáveis")
+        elif vrf1 == 5:
+            v = var.split('[')[0]
+            print("Erro: '" + v + "' não possui esse índice")
+        elif flag and vrf2 == "A":
+            print("Erro: Variável '" + value + "' vazia")
+        elif flag and vrf2 == "B":
+            print("Erro: Variável '" + value + "' inexsitente")
+        elif flag and vrf2 == "C":
+            vl = value.split('[')[0]
+            print("Erro: '" + vl + "' não possui esse índice")
+        elif flag and vrf2 == "D":
+            vl = value.split('[')[0]
+            print("Erro: '" + vl + "' não possui essa entrada")
+        else:
+            if type_verifier(var,vrf2,self.regs) == 0:
+                print("Erro: '" + var + "' e '" + value + "' são de tipos diferentes")
+
+    def instr_read(self,tree):
+        self.instrs['read'] += 1
+        var = self.visit(tree.children[2])
+        vrf = var_verifier(var,self.regs)
+        if vrf == 3:
+            print("Erro: Variável '" + var + "' inexsitente")
+
+    def instr_write(self,tree):
+        self.instrs['write'] += 1
+        var = self.visit(tree.children[2])
+        vrf = var_verifier(var,self.regs)
+        if vrf == 3:
+            print("Erro: Variável '" + var + "' inexsitente")
+
+    def instr_if(self,tree):
+        self.instrs['if'] += 1
+        if len(tree.children) == 7:
+            self.level += 1
+            self.visit(tree.children[5])
+            self.level -= 1
+        else: self.visit(tree.children[4])
+
+    def instr_if_else(self,tree):
+        self.instrs['if_else'] += 1
+        if len(tree.children) == 5:
+            self.level += 1
+            self.visit(tree.children[3])
+            self.level -= 1
+        else: self.visit(tree.children[2])
+
+    def instr_for(self,tree):
+        self.instrs['for'] += 1
+        if len(tree.children) == 8:
+            self.level += 1
+            self.visit(tree.children[6])
+            self.level -= 1
+        else: self.visit(tree.children[4])
+
+    def instr_while(self,tree):
+        self.instrs['while'] += 1
+        if len(tree.children) == 7:
+            self.level += 1
+            self.visit(tree.children[5])
+            self.level -= 1
+        else: self.visit(tree.children[4])
+
+    def instr_repeat(self,tree):
+        self.instrs['repeat'] += 1
+        self.level += 1
+        self.visit(tree.children[2])
+        self.level -= 1
 
     def logic(self,tree):
         return self.visit_children(tree)[0]
@@ -132,12 +287,8 @@ class LPIS2_Interpreter(Interpreter):
             var = tree.children[0].value
             if tree.children[0].type == 'INT':
                 return int(var)
-            if tree.children[0].type == 'BOOLEAN':
-                if var == "true": return True
-                else: return False
-            if tree.children[0].type == 'STRING':
-                return str(var)
-            return var
+            else:
+                return var
             
         
 grammar = open('lpis2.txt').read()
--- aux.py
+++ aux.py
@@ -22,40 +22,186 @@ class Registers():
 # 1 -> variável declarada mas nao inicializada
 # 2 -> variavel declarada e inicializada
 # 3 -> variável inexistente
+# 4 -> tuplos são imutáveis
+# 5 -> estrutura não possui esse índice
+# 6 -> entrada inexistente
+
 def var_verifier(var,regs):
+    flag = 0
+    
     for i in regs.int:
         if var == i:
             if regs.int[i] == None:
                 return 1
             else : return 2
+    
     for i in regs.boolean:
         if var == i:
             if regs.boolean[i] == None:
                 return 1
             else : return 2
+    
     for i in regs.string:
         if var == i:
             if regs.string[i] == None:
                 return 1
             else : return 2
+    
+    if '[' in var:
+        v = var.split('[')[0]
+        index = var.split('[')[1].split(']')[0]
+        flag = 1
+    
     for i in regs.list:
-        if var == i:
-            if regs.list[i] == None:
-                return 1
-            else : return 2
+        if not(flag):
+            if var == i:
+                if regs.list[i] == None or regs.list[i] == []:
+                    return 1
+                else : return 2
+        else:
+            if v == i:
+                if len(regs.list[i]) > int(index): return 2
+                else: return 5
+               
     for i in regs.tuple:
-        if var == i:
-            if regs.tuple[i] == None:
-                return 1
-            else : return 2
+        if not(flag):
+            if var == i:
+                if regs.tuple[i] == None or regs.tuple == ():
+                    return 1
+                else : return 2
+        else: 
+            if v == i: return 4
+
     for i in regs.set:
-        if var == i:
-            if regs.set[i] == None:
-                return 1
-            else : return 2
+        if not(flag):
+            if var == i:
+                if regs.set[i] == None or regs.set[i] == set():
+                    return 1
+                else : return 2
+        else:
+            if v == i:
+                if len(regs.set[i]) > int(index): return 2
+                else: return 5
+
     for i in regs.dict:
-        if var == i:
-            if regs.dict[i] == None:
-                return 1
-            else : return 2
+        if not(flag):
+            if var == i:
+                if regs.dict[i] == None or regs.dict[i] == {}:
+                    return 1
+                else : return 2
+        else:
+            if v == i: return 2
     return 3
+
+# A -> variável declarada mas nao inicializada
+# B -> variável inexistente
+# C -> estrutura não possui esse índice
+# D -> entrada inexistente
+def value_verifier(value,regs):
+    flag = 0
+    
+    for i in regs.int:
+        if value == i:
+            if regs.int[i] == None:
+                return "A"
+            else:
+                value = regs.int[i] 
+                return value
+    
+    for i in regs.boolean:
+        if value == i:
+            if regs.boolean[i] == None:
+                return "A"
+            else:
+                value = regs.boolean[i] 
+                return value
+    
+    for i in regs.string:
+        if value == i:
+            if regs.string[i] == None:
+                return "A"
+            else:
+                value = regs.string[i] 
+                return value
+    
+    if not(isinstance(value,int)) and '[' in value:
+        v = value.split('[')[0]
+        index = value.split('[')[1].split(']')[0]
+        flag = 1
+    
+    for i in regs.list:
+        if not(flag):
+            if value == i:
+                if regs.list[i] == None or regs.list[i] == []:
+                    return "A"
+                else:
+                    value = regs.list[i]
+                    return value
+        else:
+            if v == i:
+                if len(regs.list[i]) > int(index): 
+                    value = regs.list[i][int(index)]
+                    return value
+                else: return "C"
+               
+    for i in regs.tuple:
+        if not(flag):
+            if value == i:
+                if regs.tuple[i] == None or regs.tuple == ():
+                    return "A"
+                else:
+                    value = regs.tuple[i] 
+                    return value
+        else: 
+            if v == i:
+                if len(regs.list[i]) > int(index):
+                    value = regs.tuple[i][int(index)] 
+                    return value
+                else: return "C"
+
+    for i in regs.set:
+        if not(flag):
+            if value == i:
+                if regs.set[i] == None or regs.set[i] == set():
+                    return "A"
+                else : 
+                    value = regs.set[i]
+                    return value
+        else:
+            if v == i:
+                if len(regs.set[i]) > int(index): 
+                    value = regs.set[i][int(index)]
+                    return value
+                else: return "C"
+
+    for i in regs.dict:
+        if not(flag):
+            if value == i:
+                if regs.dict[i] == None or regs.dict[i] == {}:
+                    return "A"
+                else: 
+                    value = regs.dict[i]
+                    return value
+        else:
+            if v == i:
+                if index in regs.dict[i]: 
+                    value = regs.dict[i][index]
+                    return value
+                else: return "D"
+    return "B"
+
+           
+# 0 -> valor com diferente tipo da variável
+def type_verifier(var,value,regs):
+    for i in regs.int:
+        if var == i:
+            if isinstance(value,int): return 1
+            else: return 0
+    for i in regs.boolean:
+        if var == i:
+            if value == 'true' or value == 'false': return 1
+            else: return 0
+    for i in regs.string:
+        if var == i:
+            if ('"' in value): return 1
+            else: return 0
