739bc49350bf1c96884cf53cda2c78a1f474c737
António Rodrigues <48989779+133Bayard@users.noreply.github.com>
2022-04-17 20:16:10 +0100

    Algum html funcional, paramos no read e write

--- analyser.py
+++ analyser.py
@@ -2,7 +2,57 @@ from lark import Lark
 from lark.visitors import Interpreter
 from aux import *
 
-class LPIS2_Interpreter(Interpreter):
+
+class LPIS2_Interpreter(Interpreter): 
+    htmli = '''
+<!DOCTYPE html>
+<html>
+<style>
+    .error {
+        position: relative;
+        display: inline-block;
+        border-bottom: 1px dotted black;
+        color: red;
+}
+    .code {
+        position: relative;
+        display: inline-block;
+}
+    .error .errortext {
+        visibility: hidden;
+        width: 200px;
+        background-color: #555;
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+        padding: 5px 0;
+        position: absolute;
+        z-index: 1;
+        bottom: 125%;
+        left: 50%;
+        margin-left: -40px;
+        opacity: 0;
+        transition: opacity 0.3s;
+}
+    .error .errortext::after {
+        content: "";
+        position: absolute;
+        top: 100%;
+        left: 20%;
+        margin-left: -5px;
+        border-width: 5px;
+border-style: solid;
+        border-color: #555 transparent transparent transparent;
+    }
+    .error:hover .errortext {
+        visibility: visible;
+        opacity: 1;
+} </style>
+<body>
+    <h2>Análise de código</h2>
+    <pre><code>
+''' 
+
     def __init__(self):
         self.regs = Registers()
         self.instrs = {'atrib': 0, 'read': 0, 'write': 0, 'if': 0, 'if_else': 0, 'for': 0, 'while': 0, 'repeat': 0}
@@ -12,17 +62,22 @@ class LPIS2_Interpreter(Interpreter):
 
     def start(self,tree):
         self.visit(tree.children[0])
+        self.htmli += '<p class="code">' + tree.children[1].value + '</p>'
         self.visit_children(tree.children[2])
         self.output['regs'] = self.regs.to_string()
         self.output['instrs'] = self.instrs
         self.output['ifs'] = self.ifs
+        self.output['html'] = self.htmli
         return(self.output)
 
     def decls(self,tree):
         self.visit_children(tree)
+          
+
 
     def decl(self,tree):
         self.visit_children(tree)
+        self.htmli += ';</p>\n'
 
     def decl_int(self,tree):
         var = tree.children[1].value
@@ -49,9 +104,16 @@ class LPIS2_Interpreter(Interpreter):
                         self.regs.int[var] = value
             else:
                 self.regs.int[var] = value
-        else: 
+            self.htmli += '<p class="code">' + tree.children[0].value + ' ' + var + ' = ' + str(value)
+        else:
+            self.htmli += '<p class="code">' + tree.children[0].value + ' ' + var
             value = None
             self.regs.int[var] = value
+        
+         
+            
+            
+
 
     def decl_boolean(self,tree):
         var = tree.children[1].value
@@ -78,9 +140,12 @@ class LPIS2_Interpreter(Interpreter):
                         self.regs.boolean[var] = value
             else:
                 self.regs.boolean[var] = value
-        else: 
+            self.htmli += '<p class="code">' + tree.children[0].value + ' ' + var + ' = ' + value
+        else:
+            self.htmli += '<p class="code">' + tree.children[0].value + ' ' + var
             value = None
             self.regs.boolean[var] = value
+        
 
     def decl_string(self,tree):
         var = tree.children[1].value
@@ -107,12 +172,15 @@ class LPIS2_Interpreter(Interpreter):
                         self.regs.string[var] = value
             else:
                 self.regs.string[var] = value
+            self.htmli += '<p class="code">' + tree.children[0].value + ' ' + var + ' = ' + value
         else: 
             value = None
             self.regs.string[var] = value
+            self.htmli += '<p class="code">' + tree.children[0].value + ' ' + var
 
     def decl_list(self,tree):
         name = tree.children[1].value
+        self.htmli += '<p class="code">' + tree.children[0].value + ' ' + name
         if len(tree.children) == 3:
             var = self.visit(tree.children[2])
         else: var = []
@@ -120,6 +188,7 @@ class LPIS2_Interpreter(Interpreter):
 
     def decl_tuple(self,tree):
         name = tree.children[1].value
+        self.htmli += '<p class="code">' + tree.children[0].value + ' ' + name
         if len(tree.children) == 3:
             var = self.visit(tree.children[2])
         else: var = ()
@@ -127,17 +196,21 @@ class LPIS2_Interpreter(Interpreter):
 
     def decl_set(self,tree):
         name = tree.children[1].value
+        self.htmli += '<p class="code">' + tree.children[0].value + ' ' + name
         if len(tree.children) == 3:
             var = self.visit(tree.children[2])
         else: var = {}
         self.regs.set[name] = var
+        
 
     def decl_dict(self,tree):
         name = tree.children[1].value
+        self.htmli += '<p class="code">' + tree.children[0].value + ' ' + name
         if len(tree.children) == 3:
             var = self.visit(tree.children[2])
         else: var = dict()
         self.regs.dict[name] = var
+        
 
     def decl_atrib(self,tree):
         return self.visit(tree.children[1])
@@ -145,34 +218,76 @@ class LPIS2_Interpreter(Interpreter):
     def decl_atrib_list(self,tree):
         res = []
         i = 2
-        while i < len(tree.children):
-            res.append(self.visit(tree.children[i]))
-            i += 2
+        j = 3
+        self.htmli += ' ' + tree.children[0] + ' ' + tree.children[1]
+        if len(tree.children) > 3:
+            while i < len(tree.children):
+                value = self.visit(tree.children[i])
+                res.append(value)
+                self.htmli += str(value)
+                self.htmli += tree.children[j]
+                i += 2
+                j += 2
+        else:
+            self.htmli += tree.children[2]
+
         return res
 
     def decl_atrib_tuple(self,tree):
-        aux = []
+        res = []
         i = 2
-        while i < len(tree.children):
-            aux.append(self.visit(tree.children[i]))
-            i += 2
-        return tuple(aux)
+        j = 3
+        self.htmli += ' ' + tree.children[0] + ' ' + tree.children[1]
+        if len(tree.children) > 3:
+            while i < len(tree.children):
+                value = self.visit(tree.children[i])
+                res.append(value)
+                self.htmli += str(value)
+                self.htmli += tree.children[j]
+                i += 2
+                j += 2
+        else:
+            self.htmli += tree.children[2]
+
+        return tuple(res)
 
     def decl_atrib_set(self,tree):
         res = set()
         i = 2
-        while i < len(tree.children):
-            res.add(self.visit(tree.children[i]))
-            i += 2
+        j = 3
+        self.htmli += ' ' + tree.children[0] + ' ' + tree.children[1]
+        if len(tree.children) > 3:
+            while i < len(tree.children):
+                value = self.visit(tree.children[i])
+                res.add(value)
+                self.htmli += str(value)
+                self.htmli += tree.children[j]
+                i += 2
+                j += 2
+        else:
+            self.htmli += tree.children[2]
+
         return res
 
     def decl_atrib_dict(self,tree):
         res = dict()
         i = 2
-        while i < len(tree.children):
-            elem = tree.children[i]
-            res[str(self.visit(elem)[0])] = self.visit(elem)[2]
-            i += 2
+        j = 3
+        self.htmli += ' ' + tree.children[0] + ' ' + tree.children[1]
+        if len(tree.children) > 3:
+            while i < len(tree.children):
+                elem = tree.children[i]
+                key = self.visit(elem)[0]
+                dp = self.visit(elem)[1]
+                value = self.visit(elem)[2]
+                res[str(key)] = value
+                self.htmli += key + dp + ' ' + str(value)
+                self.htmli += tree.children[j]
+                i += 2
+                j += 2
+    
+        else:
+            self.htmli += tree.children[2]
         return res
 
     def instrs(self,tree):
@@ -203,7 +318,7 @@ class LPIS2_Interpreter(Interpreter):
         elif flag and vrf2 == "A":
             print("Erro: Variável '" + value + "' vazia")
         elif flag and vrf2 == "B":
-            print("Erro: Variável '" + value + "' inexsitente")
+            print("Erro: Variável '" + value + "' inexistente")
         elif flag and vrf2 == "C":
             vl = value.split('[')[0]
             print("Erro: '" + vl + "' não possui esse índice")
@@ -213,20 +328,25 @@ class LPIS2_Interpreter(Interpreter):
         else:
             if type_verifier(var,vrf2,self.regs) == 0:
                 print("Erro: '" + var + "' e '" + value + "' são de tipos diferentes")
+        self.htmli += '\n<p class="code">' + var +' = ' + value + tree.children[2] + '</p>'
 
     def instr_read(self,tree):
         self.instrs['read'] += 1
-        var = self.visit(tree.children[2])
+        var = tree.children[2].value
         vrf = var_verifier(var,self.regs)
         if vrf == 3:
-            print("Erro: Variável '" + var + "' inexsitente")
+            print("Erro: Variável '" + var + "' inexistente")
+        else:
+            self.htmli += '\n<p class="code">' + tree.children[0] + tree.children[1] + var + tree.children[3] + tree.children[4] + '</p>'
 
     def instr_write(self,tree):
         self.instrs['write'] += 1
         var = self.visit(tree.children[2])
         vrf = var_verifier(var,self.regs)
         if vrf == 3:
-            print("Erro: Variável '" + var + "' inexsitente")
+            print("Erro: Variável '" + var + "' inexistente")
+        else:
+            self.htmli += '\n<p class="code">' + tree.children[0] + tree.children[1] + str(var) + tree.children[3] + tree.children[4] + '</p>'
 
     def instr_if(self,tree):
         self.instrs['if'] += 1
@@ -289,14 +409,24 @@ class LPIS2_Interpreter(Interpreter):
                 return int(var)
             else:
                 return var
-            
+
+htmlf = '''
+</code></pre>
+</body>
+</html>
+'''
         
 grammar = open('lpis2.txt').read()
 code = open('test_code.txt').read()
 
+
 p = Lark(grammar)
 parse_tree = p.parse(code)
 data = LPIS2_Interpreter().visit(parse_tree)
+
+with open("body.html",'w',encoding = 'utf-8') as f:
+   f.write(data['html'])
+   f.write(htmlf)
 
 for key in data:
     if key == "regs":
